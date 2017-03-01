9a199eaa2b8a8b0db3e727e1db29f3cf0f6b9c9f
tico2303 <tico82003@gmail.com>
2017-03-01 02:42:00 -0800

    refining search

--- KNN_Classifier/search.py
+++ KNN_Classifier/search.py
@@ -7,25 +7,46 @@ class Search(object):
 		""" data is an instance of the Data class
 		validator is an instance of the validator class initailize with a
 		classifier"""
-		
+
 		self.Data = data
-		self.validator = validator
+		self.validator = Validator(data)
 	def search(self):
 		raise NotImplemented
 
 
 class ForwardSelection(Search):
 
+	def get_features(self,node):
+		results = []
+		col_nums = self.data.shape[1]
+		for i in range(col_nums):
+			new_node = []
+			new_node = [x for x in node]
+			for j in range(len(node) +1):
+				if 	i not in new_node:
+					new_node.append(i)
+					results.append(new_node)
+		print "results: ", results
+		#print "\n"
+		return results
+
+
 	def search(self):
-		feature_indices = []
-		col_nums = self.Data.data.shape[1]
-		explored = []
-		curr_min_cost = 9999
-		q = [1]
-		while len(q) >0:
-			curr_feature = q.pop()
-			for feature in range(curr_feature, col_nums)
-				self.validator.validate
+		q = [[]]
+		best_accuracy = (0,None)
+		while q:
+			node = q.pop()
+			if len(node) == col_nums:
+				break
+			x = 0
+			for state in get_states(node):
+				accuracy = self.validator.validate()
+				if best_accuracy[0] <accuracy:
+					best_accuracy[0] = accuracy
+					best_accuracy[1] = state
+				#print state
+			q.append(best_accuracy[1])	
+
 
 
 
